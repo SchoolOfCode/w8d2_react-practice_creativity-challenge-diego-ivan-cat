aaa8c7fe30c4b2333db928b26d1adc3c210df5ad
Diego Russo <diego25.russo@gmail.com>
2022-01-11 11:04:58 +0000

    start TodoList

---
+++ my-app/src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div> 
+            <header>
+                <h1>To Do list </h1>
+            </header>
+        
+        </div>
+    )
+}
